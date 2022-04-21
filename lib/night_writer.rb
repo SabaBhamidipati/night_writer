7a7c69dae8503485cc44d2d851ee0edae284eb71
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-04-21 19:29:42 -0400

    Initial Commit: create EnglishReader class to read and write files

---
+++ lib/night_writer.rb
@@ -0,0 +1,16 @@
+require_relative 'english_reader'
+
+enghlishreader = EnglishReader.new(ARGV[0], ARGV[1])
+# require "pry"; binding.pry
+
+
+
+
+
+# file.read returns string of char of whole file, readlines returns array of strings, and you can convert each string into braille one character at at time  and return
+# there's logic in out put to limit to 80 char using hash dictionary (Module - translator - assigns letters in eglish text to arratys of char that represent Braille)
+# message txt comes in, code converts, and outputs in braille.txt file
+#create english reader to convert to braille characters
+# Iteration 3 convert braille reader to english reader
+# call chomp everytime to convert x.first.chomp.length
+#Create engish reader class
