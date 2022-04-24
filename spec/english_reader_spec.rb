047c5c5e74e4d7cb0da333d5a4e26234299a33c5
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-04-24 12:12:54 -0400

    Initial commit: test for reader existence

--- spec/english_reader_spec.rb
+++ spec/english_reader_spec.rb
@@ -1,1 +1,14 @@
+require './lib/english_reader'
 
+RSpec.describe EnglishReader do
+
+  before :each do
+    @english_file_path = './spec/testing_txt_files/english_chars.txt'
+    @braille_file_path = './spec/testing_txt_files/braille_chars.txt'
+    @english_reader = EnglishReader.new(@english_file_path, @braille_file_path)
+  end
+
+  it 'exists' do
+    expect(@english_reader).to be_a(EnglishReader)
+  end #not printing file name in message for braille 
+end
